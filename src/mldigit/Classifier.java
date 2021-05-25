5b2b4e88364c860948c3f5ea88765d9abceaf59d
Mathgo12 <sur123app@gmail.com>
2021-05-25 15:51:02 -0700

    Final commit 5

--- src/mldigit/Classifier.java
+++ src/mldigit/Classifier.java
@@ -29,69 +29,96 @@ import org.nd4j.evaluation.classification.Evaluation;
 import java.io.*;
 
 public class Classifier {
-
-	private static String path = "mnist_png\\";
-	private static int batches = 10;
+	 															
+	private static String path = "mnist_png\\";  //path for loading data
+	private static int batches = 10;                    
 	private static int epochs = 1;
-	private static MultiLayerNetwork network;
-	private static int randomSeed = 56543219;
+	private static MultiLayerNetwork network;   //Neural network 
+	private static int randomSeed = 56543219;    
 	
 	public static DataSetIterator dataProcessor(String addedPath, int numSamples){
+		
+		//An exception may be thrown when loading an image given a path.
 	    try {
-			ParentPathLabelGenerator labelMaker = new ParentPathLabelGenerator();
+			ParentPathLabelGenerator labelMaker = new ParentPathLabelGenerator(); //takes name of folder and sets it to the label
+			
+			/** This RecordReader objects converts images into a series of Record objects. 
+			 *  Record objects are single vectors. Each element contains a pixel value. **/
 		    RecordReader recordReader = new ImageRecordReader(28,28, labelMaker); //28x28 RGB images
 			recordReader.initialize(new FileSplit(new File(path + addedPath)));
 			
-			DataSetIterator tempDataset = new RecordReaderDataSetIterator.Builder(recordReader, numSamples)
-		    		.classification(1,10) //10 outcomes
-		    		.preProcessor(new ImagePreProcessingScaler())
+			
+			/**A DataSet object holds data. The input features are stored as a matrix, and the output labels are 
+			 * stored as a different matrix. Each associate with one another so that each training example (a vector of pixel values)
+			 * is linked to a single output (label).
+			 * 
+			 *  A DataSet object holds a single batch of data. A DataSetIterator allows to iterate across multiple DataSet objects.
+			 *  In the first DataSetIterator, we put all the training data into a single DataSet which is why the batch size is 
+			 *  numSamples. This way, we can easily make any modifications using this DataSet object.**/
+			
+			DataSetIterator tempDataset = new RecordReaderDataSetIterator.Builder(recordReader, numSamples)  
+		    		.classification(1,10) //We want a classification iterator. 1 refers to the index of the labels (index 0 is all the features) and 10 is the 10 outputs 0-9.
+		    		.preProcessor(new ImagePreProcessingScaler()) //Its easier to work with pixel values 0-1 instead of 0-255, so that values are scaled down.
 		    		.build();
 		    
-		    DataSet train = tempDataset.next();
-		    INDArray inputs = train.getFeatures().reshape(new int[]{numSamples, 784}); //28 * 28 = 784
-		    INDArray outputs = train.getLabels();
+			/** There's only 1 DataSet object since we used all the training examples for the batch size. **/
+		    DataSet data = tempDataset.next();
 		    
-		    DataSet dataset = new DataSet(inputs, outputs);
-		    dataset.shuffle();
-		    DataSetIterator dataSetIterator = new ListDataSetIterator<DataSet>(dataset.asList(), batches);
+		    /**An INDArray is a more advanced version of the general java array that has a variety of methods to work with for data manipulation.
+		     * The 'inputs' variable includes the features from the DataSet object. The model can only work with 2D data, so the inputs are scaled
+		     * to be a 2-D matrix. 784 represents the total number of pixels per image. NumSamples is the total number of images being used.**/
+		    
+		    INDArray inputs = data.getFeatures().reshape(new int[]{numSamples, 784}); 
+		    INDArray outputs = data.getLabels();
+		    
+		    DataSet dataset = new DataSet(inputs, outputs); //Create new DataSet object
+		    
+		    /**Randomize the order of each training example and label. We need to do this so that 
+		     * the model can train properly without seeing the same type of data multiple times. **/
+		    dataset.shuffle(); 
+		    
+		    //DataSetIterator object which the model can work with, split into more batches
+		    DataSetIterator dataSetIterator = new ListDataSetIterator<DataSet>(dataset.asList(), batches); //DataSetIterator object which the model can work with, split into more batches
 		    
 			return dataSetIterator;
 			
 		} catch (Exception e) {		
 			e.printStackTrace();
 		}
-	    return null;
+	    
+	    return null;  
 		
     } 
 	
+	/** Method for both training and testing the model **/
 	public static void trainTestModel(DataSetIterator trainIterator, DataSetIterator testIterator) {
 		
 		System.out.print("Build Model...");
         MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()   //Building Neural Network Model
-                .seed(randomSeed)
+                .seed(randomSeed) 
                 .updater(new Nesterovs(0.006, 0.9)) //0.006 is the learning rate, 0.9 is Nesterov's momentum 
-                //.l2(0.02) //L2 Regularization plus lambda (regularization coefficient)
+                .l2(0.001) //L2 Regularization plus lambda (regularization coefficient)
                 .list()
                 .layer(new DenseLayer.Builder()
-                        .nIn(784)
+                        .nIn(784)   //Each input image is interpreted as a row of pixel values, and there are 784 pixels
                         .nOut(1000) //1000 nodes in hidden layer
-                        .activation(Activation.RELU)
-                        .weightInit(WeightInit.NORMAL)
+                        .activation(Activation.RELU)  //Activation function (eliminates possible negative pixel values)
+                        .weightInit(WeightInit.UNIFORM) //Internal model parameters are randomized uniformly
                         .build())
-                .layer(new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                .layer(new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD) //The loss function for assessing how well the model is predicting for each training example.
                         .nIn(1000)
                         .nOut(10)
                         .activation(Activation.SOFTMAX) //turns neural network outputs to probabilities that the input data is a 0,1,2, etc.
-                        .weightInit(WeightInit.NORMAL)
+                        .weightInit(WeightInit.UNIFORM) 
                         .build())
                 .build();
 
-        network = new MultiLayerNetwork(conf);
-        network.init();
+        network = new MultiLayerNetwork(conf); //Apply the neural network configuration to the model
+        network.init();  //Initialize the model before fitting the training data
         //Print score every 500 iterations
         System.out.print("Train Model...");
         
-        //evaluate model
+        //evaluate model for each epoch
         for (int i = 0; i < epochs; i++) {
             network.fit(trainIterator);
             System.out.println("Completed epoch " + i);
@@ -105,15 +132,17 @@ public class Classifier {
 	
 	public static int prediction(File savedImagePath, MultiLayerNetwork model) throws IOException{
 		
-        NativeImageLoader loader = new NativeImageLoader(28, 28, 1);
+        NativeImageLoader loader = new NativeImageLoader(28, 28, 1);  //Loads images and allows for obtaining matrices. Larger images are compressed.
  
-        INDArray image = loader.asMatrix(savedImagePath).reshape(new int[]{1, 784});
-        DataNormalization scalar = new ImagePreProcessingScaler(0, 1);
-        scalar.transform(image);
+        INDArray image = loader.asMatrix(savedImagePath).reshape(new int[]{1, 784});  //One image is a single array with 784 pixel values 
+        DataNormalization scalar = new ImagePreProcessingScaler();  //Each image pixel is scaled to a number from 0 to 1.
+        scalar.transform(image); //applies the scaler
         INDArray output = model.output(image);
-        double highest = output.getFloat(0);
+        double highest = output.getFloat(0); 
         int bestValue = 0;
         
+        /** This code computes the largest value in the output array, which includes decimal values for how confident 
+         * the model is for each prediction (0,1,2, ...). The highest value is likely to be associated with the **/
         for(int i = 0; i<output.length(); i++)  // Prediction confidences (all add up to 1). The value closest to 1 is predicted. 
         {
         	if(output.getFloat(i) > highest) { 
--- src/mldigit/GUI.java
+++ src/mldigit/GUI.java
@@ -35,97 +35,98 @@ public class GUI  {
     	
     	try {
     	
-	        /** initializes JFrame and sets attributes */ 
+	    /** initializes JFrame and sets attributes */ 
 		JFrame frame = new JFrame();
 	
-	        frame.setSize(600,600);
-	        frame.setResizable(false);
-	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        frame.setTitle("Hand-Written Digit Recognition: numbers 0-9");
+	    frame.setSize(600,600);
+	    frame.setResizable(false);
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.setTitle("Hand-Written Digit Recognition: numbers 0-9");
 		
-	        //???
-	        File path = new File("modelSave\\savedModel.zip");
-	        model = ModelSerializer.restoreMultiLayerNetwork(path);
+	    /** load saved model **/
+	    File path = new File("modelSave\\savedModel.zip");
+	    model = ModelSerializer.restoreMultiLayerNetwork(path);
 	
-	        /** initializes JPanels on JFrame */
+	    /** initializes JPanels on JFrame */
 		JPanel panel = new JPanel();
-	        JPanel panel2 = new JPanel();
+	    JPanel panel2 = new JPanel();
 	        
 		/** initliazes and centers label that outputs predicted value */
 		label = new JLabel("PREDICTED VALUE HERE",SwingConstants.CENTER);
 	        
 		/** increases fontsize */
 		Font labelFont = label.getFont();
-	        label.setFont(new Font(labelFont.getName(), Font.PLAIN, 15));
+	    label.setFont(new Font(labelFont.getName(), Font.PLAIN, 15));
 		
 		
 		/** intializes buttons for classifying and erasing the drawn images */
-	        button = new JButton("Classify Image");
-	        button2 = new JButton("Erase Image");
+        button = new JButton("Classify Image");
+        button2 = new JButton("Erase Image");
 	        
 		/** adds listeners to buttons */
-	        button.addActionListener(new ButtonListeners());
-	        button2.addActionListener(new ButtonListeners());
+        button.addActionListener(new ButtonListeners());
+        button2.addActionListener(new ButtonListeners());
 
-	        /** initalizes scribblePane JPanel, sets border */
-	        scribblePane = new ScribblePane();
-	        scribblePane.setBorder(new BevelBorder(BevelBorder.LOWERED);
-	
-		/** initializes GridBagLayout */
-	        GridBagLayout layout = new GridBagLayout();
+        /** initalizes scribblePane JPanel, sets border */
+        scribblePane = new ScribblePane();
+        scribblePane.setBorder(new BevelBorder(BevelBorder.LOWERED));
+
+        /** initializes GridBagLayout */
+        GridBagLayout layout = new GridBagLayout();
 		
 		
-	        /** sets borders to JPanels on JFrame */
+	    /** sets borders to JPanels on JFrame */
 		panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
-	        panel2.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
+	    panel2.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
 	
 	        /** sets panel's layout as GridBagLayout */
 		panel.setLayout(layout);
 		
 		/** adds label to panel2 with BorderLayout */
-	        panel2.setLayout(new BorderLayout());
-	        panel2.add(label, BorderLayout.SOUTH);
+        panel2.setLayout(new BorderLayout());
+        panel2.add(label, BorderLayout.SOUTH);
 		
 		
-	        /** intializes constants for the GridBagLayout */
+	    /** intializes constants for the GridBagLayout */
 		GridBagConstraints gbc = new GridBagConstraints();
 	        
 		/** fills horizontally if display area is larger than requested size */		       
 		gbc.fill = GridBagConstraints.HORIZONTAL;
+	
 		
 		/** sets scribblePane to row 0, column 0 */
-	        gbc.gridx = 0;
-	        gbc.gridy = 0;
-	       
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+       
 		panel.add(scribblePane, gbc);
-		
-		
-	        /** sets panel2 to row 1, column 0 */
-	        gbc.gridx = 1;
-	        gbc.gridy = 0;
+	
+	
+        /** sets panel2 to row 1, column 0 */
+        gbc.gridx = 1;
+        gbc.gridy = 0;
 	        
 		panel.add(panel2,gbc);
 	
 		
 		/** sets button to row 0, columumn 1 */
-	        gbc.gridx = 0;
-	        gbc.gridy = 2;
-	        
-	        panel.add(button, gbc);
-	
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        
+        panel.add(button, gbc);
+
 		/** sets button2 to row 1, column 1 */
-	        gbc.gridx = 1;
-	        gbc.gridy = 1;
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+
+        panel.add(button2, gbc);
+
+
+        /** adds panel to the JFrame and makes it visible */
 	
-	        panel.add(button2, gbc);
-	
-	
-	        /** adds panel to the JFrame and makes it visible */
-		
-	        frame.add(panel);
-	        frame.setVisible(true);
-	
-	
+        frame.add(panel);
+        frame.setVisible(true);
+
+
 
     	}
     	
@@ -145,9 +146,9 @@ public class GUI  {
 			if(e.getSource() == button) {
 	            	try {
 		                /** increments i, the number in saved file name */
-				i++;
+	            		i++;
 				
-				/** saves current image on scribblePane with number i */
+	            		/** saves current image on scribblePane with number i */
 		                saveDrawing(i);
 				
 	                                /** loads the recently saved file */
@@ -157,7 +158,7 @@ public class GUI  {
 		        		int outputVal = Classifier.prediction(imagePath, model);
 	
 		                /** sets label text to predictedvalue from model */
-				label.setText("PREDICTED VALUE: " + outputVal);
+		        		label.setText("PREDICTED VALUE :  " + outputVal);
 	            	}
 	            	
 	            	catch(IOException exception) {
@@ -174,10 +175,10 @@ public class GUI  {
 				scribblePane.erase();
 				
 				/** deletes saved file of the image */
-	                        deleteFile("save"+i+".png");
+	            deleteFile("save"+i+".png");
 			        
 				/** clears the predicted value from the label */
-	                        label.setText("");
+	            label.setText("PREDICTED VALUE HERE");
 	        		
 	        	}
 	        }
@@ -187,14 +188,14 @@ public class GUI  {
     public void saveDrawing(int i){
 
         /** creates local buffer image, sets to image on scribblePane using getter method */
-	BufferedImage imagebuf = null;
+    	BufferedImage imagebuf = null;
         imagebuf = scribblePane.getImage();
         
         try {
-	    /** saves imagebuf as a file in folder drawnImages with number i */
+        	/** saves imagebuf as a file in folder drawnImages with number i */
             ImageIO.write(imagebuf, "png", new File("drawnImages\\save" + i +".png"));
         
-	} catch (Exception e) {
+        } catch (Exception e) {
            e.printStackTrace();
         }
     }
@@ -208,13 +209,13 @@ public class GUI  {
     /** returns downscaled buffered image on ScribblePane with a new width and height */	
     public static BufferedImage resize(BufferedImage img, int newW, int newH) {
         
-	/** creates an image object with new dimensions and scaling option of SCALE_SMOOTH */
-	Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
-	
-	/** creates new buffered image with the given dimensions */
-        BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
-
-	/** draws the image on the new buffered image */
+		/** creates an image object with new dimensions and scaling option of SCALE_SMOOTH */
+		Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
+		
+		/** creates new buffered image with the given dimensions */
+	    BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
+	    
+	    /** draws the image on the new buffered image */
         Graphics2D g2d = dimg.createGraphics();
         g2d.drawImage(tmp, 0, 0, null);
         g2d.dispose();
@@ -238,11 +239,11 @@ class ScribblePane extends JPanel {
 
     public ScribblePane() {
 	    
-	/** sets dimensions of JPanel to be sxs  */ 
+    	/** sets dimensions of JPanel to be sxs  */ 
         s = 200;
         this.setPreferredSize(new Dimension(s, s));
 	    
-	/** creates a buffered image on top of the JPanel with same dimensions and fills it with black */    
+        /** creates a buffered image on top of the JPanel with same dimensions and fills it with black */    
         bufferImage = new BufferedImage(s, s, BufferedImage.TYPE_INT_ARGB);
         g = (Graphics2D) bufferImage.getGraphics();
         g.setColor(Color.BLACK);
@@ -277,18 +278,17 @@ class ScribblePane extends JPanel {
     /** Draw from the last point to this point, then remember new point */
     public void lineto(int x, int y) {
 
-	/**updates the scribblePane JPanel */ 
+    	/**updates the scribblePane JPanel */ 
         this.clear();
         
-	/** draws the line */
+        /** draws the line */
 	
-	g.setColor(color);
+        g.setColor(color);
         ((Graphics2D) g).setStroke(new BasicStroke(20));
         g.drawLine(last_x, last_y, x, y);
         moveto(x, y);
 	    
-	/** draws the line on the bufferImage */ 
-
+        /** draws the line on the bufferImage */ 
         g.drawImage(bufferImage,0,0, null);
 
     }
@@ -316,14 +316,14 @@ class ScribblePane extends JPanel {
     /** clears the bufferedImage */
     public void erase(){
           
-	/** clears the grahics panel */
+    	/** clears the grahics panel */
         g.clearRect(0,0,s,s);
 	    
-	/** creates new local bufferedImage */
+        /** creates new local bufferedImage */
         BufferedImage newBufferImage = new BufferedImage(s, s,
-                BufferedImage.TYPE_INT_ARGB);
+        BufferedImage.TYPE_INT_ARGB);
         
-	/** sets it equal to global buffered image reference to clear the screen */
+        /** sets it equal to global buffered image reference to clear the screen */
         bufferImage = newBufferImage;
         g = (Graphics2D) bufferImage.getGraphics();
         g.setColor(Color.BLACK);
